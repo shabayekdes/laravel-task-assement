26adc05344c9de8bcc1255e80f0c5c0762d821f6
Esmail Shabayek <esmail.shabayek@gmail.com>
2023-04-13 01:51:42 +0200

    Convert dashboard page to ajax

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -11,9 +11,54 @@ class DashboardController extends Controller
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function index(Request $request)
     {
         $users = User::withCount('tasks')->where('type', UserType::TYPE_USER->value)->orderByDesc('tasks_count')->paginate(10);
         return view('dashboard', compact('users'));
     }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function getUsersListAjax(Request $request)
+    {
+        ## Read value
+        $draw = $request->get('draw');
+
+        // Total records
+        $totalRecords = User::select('count(*) as allcount')
+            ->count();
+
+        $totalRecordsWithFilter = User::select('count(*) as allcount')
+            ->where('type', UserType::TYPE_USER->value)
+            ->count();
+
+        // Fetch records
+        $records = User::withCount('tasks')
+            ->where('type', UserType::TYPE_USER->value)
+            ->orderByDesc('tasks_count')
+            ->take(10)
+            ->get();
+
+
+        $data_arr = [];
+        foreach ($records as $record) {
+            $data_arr[] = [
+                "id" => $record->id,
+                "user_name" => $record->name,
+                "user_email" => $record->email,
+                "tasks_count" => $record->tasks_count
+            ];
+        }
+        $response = [
+            "draw" => intval($draw),
+            "iTotalRecords" => $totalRecords,
+            "iTotalDisplayRecords" => $totalRecordsWithFilter,
+            "aaData" => $data_arr
+        ];
+        return response()->json($response);
+    }
+
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
         <div class="py-8">
             <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4">
                 <div class="inline-block min-w-full shadow rounded-lg">
-                    <table class="min-w-full leading-normal">
+                    <table class="min-w-full leading-normal" id="kt_datatable">
                         <thead>
                         <tr>
                             <th scope="col"
@@ -25,31 +25,58 @@
                             </th>
                         </tr>
                         </thead>
-                        <tbody>
-                        @foreach ($users as $user)
-                            <tr>
-                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                    <p class="text-gray-900 whitespace-no-wrap">
-                                        {{ $user->name }}
-                                    </p>
-                                </td>
-                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                    <p class="text-gray-900 whitespace-no-wrap">
-                                        {{ $user->email }}
-                                    </p>
-                                </td>
-                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                    <p class="text-gray-900 whitespace-no-wrap">
-                                        {{ $user->tasks_count }}
-                                    </p>
-                                </td>
-                            </tr>
-                        @endforeach
-
-                        </tbody>
                     </table>
                 </div>
             </div>
         </div>
     </div>
+    <x-slot name="scripts">
+        <script type="text/javascript">
+            $(document).ready(function () {
+                $.fn.dataTable.ext.classes.sPageButton = 'relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150';
+
+                // begin first table
+                let table = $('#kt_datatable').DataTable({
+                    responsive: true,
+                    processing: true,
+                    serverSide: true,
+                    ajax: "{{ route('ajax.users.list') }}",
+                    ordering: false,
+                    searching: false,
+                    paging: false,
+                    info: false,
+                    columnDefs: [
+                        {
+                            targets: 0,
+                            data: 'user_name',
+                            className: 'px-5 py-5 border-b border-gray-200 bg-white text-sm',
+                            render: function (data, type, full, meta) {
+                                return `<p class="text-gray-900 whitespace-no-wrap">${data}</p>`
+                            }
+                        },
+                        {
+                            targets: 1,
+                            data: 'user_email',
+                            className: 'px-5 py-5 border-b border-gray-200 bg-white text-sm',
+                            render: function (data, type, full, meta) {
+                                return `<p class="text-gray-900 whitespace-no-wrap">${data}</p>`
+                            }
+                        },
+                        {
+                            targets: 2,
+                            data: 'tasks_count',
+                            className: 'px-5 py-5 border-b border-gray-200 bg-white text-sm',
+                            render: function (data, type, full, meta) {
+                                return `<p class="text-gray-900 whitespace-no-wrap">${data}</p>`
+                            }
+                        }
+                    ],
+                });
+
+                setInterval(function () {
+                    table.ajax.reload(null, false);
+                }, 3000);
+            });
+        </script>
+    </x-slot>
 </x-app-layout>
